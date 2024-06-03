1a383377ae618c2496cf348611f342a2c15e89a9
Caleb Givens <ezio2468onpsn@gmail.com>
2024-06-03 04:46:37 -0400

    Extending JFrame for use by entry point class

--- src/Shodan.java
+++ src/Shodan.java
@@ -1,7 +1,15 @@
 import javax.swing.JFrame;
 
 public class Shodan extends JFrame {
-    public static void main(String[] args) {
 
+    public Shodan(String title) {
+        super(title);
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        Shodan program = new Shodan("Window");
     }
 }
