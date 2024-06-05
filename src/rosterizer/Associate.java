8eb9a328ba88e283ac9e4b06efabeab17ff3e1b4
Caleb Givens <ezio2468onpsn@gmail.com>
2024-06-05 06:34:43 -0400

    Replaced associate role enumerated type with binary flags

--- src/rosterizer/Associate.java
+++ src/rosterizer/Associate.java
@@ -5,23 +5,22 @@ import java.util.List;
 
 public class Associate {
 
-    public enum TrainedRole {
-        AMBASSADOR,
-        END_OF_LINE,
-        PROBLEM_SOLVE,
-        WATERSPIDER,
-        REFURB,
-        WHD,
-        RECOVERY,
-        OUTBOUND,
-        SHOE_PROCESSING,
-        ICQA,
-        TDR,
-        PIT
-    }
+    public static int ROLE_AMBASSADOR = 0b000000000001;
+    public static int ROLE_EOL = 0b000000000010;
+    public static int ROLE_PS = 0b000000000100;
+    public static int ROLE_WATERSPIDER = 0b000000001000;
+    public static int ROLE_REFURB = 0b000000010000;
+    public static int ROLE_WHD = 0b000000100000;
+    public static int ROLE_RECOVERY = 0b000001000000;
+    public static int ROLE_OUTBOUND = 0b000010000000;
+    public static int ROLE_SHOES = 0b000100000000;
+    public static int ROLE_ICQA = 0b001000000000;
+    public static int ROLE_TDR = 0b010000000000;
+    public static int ROLE_PIT = 0b100000000000;
+
 
     private final String LOGIN;
-    private final ArrayList<TrainedRole> ROLES = new ArrayList<>();
+    private int roles = 0;
     private double averageRate = 0.0;
     private double rate = 0.0;
     private double inferredTime = 0.0;
@@ -32,15 +31,13 @@ public class Associate {
     public double getRate() { return rate; }
     public double getAverageRate() { return averageRate; }
     public double getInferredTime() { return inferredTime; }
-    public List<TrainedRole> getRoles() { return ROLES; }
+    public boolean hasRole(int role) { return false; }
 
     public void setRate(double rate) { this.rate = rate; }
     public void setAverageRate(double averageRate) { this.averageRate = averageRate; }
     public void setInferredTime(double inferredTime) { this.inferredTime = inferredTime; }
 
-    public void addTrainedRole(TrainedRole newRole) {
-        if(ROLES.contains(newRole))
-            return;
-        ROLES.add(newRole);
+    public void addTrainedRole(int newRole) {
+        roles = roles | newRole;
     }
 }
