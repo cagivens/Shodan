2a43002cc32580d1b91facd12ca1db6ae4dea25d
Caleb Givens <ezio2468onpsn@gmail.com>
2024-06-20 05:38:10 -0400

    Added CSV export from Roster.kt

--- src/Shodan.kt
+++ src/Shodan.kt
@@ -3,32 +3,33 @@ import fclm.ProcessWatchdog
 import fclm.TOTWatchdog
 import rosterizer.Roster
 import java.awt.Dimension
+import java.awt.GridLayout
 import javax.swing.*
 import javax.swing.filechooser.FileSystemView
 
 
 fun main() {
     val window = JFrame("Shodan")
-    val roster = Roster(listOf("caleigiv", "todpaula"))
-    val sheet = HashMap<Char, Array<JPanel>>()
+    val roster = Roster(listOf("caleigiv", "todpaula", "mickello", "viraubre", "idonpowe"))
 
-    // Initializing menu bar
-    val menuBar = JMenuBar()
-    val fileMenu = JMenu("File")
-    val rosterMenu = JMenu("Roster")
-    val watchdogMenu = JMenu("Watchdog")
-    // Adding menu items to menu bar
-    menuBar.add(fileMenu)
-    menuBar.add(rosterMenu)
-    menuBar.add(watchdogMenu)
+    val buttonList = listOf(JButton("Import SSPOT"), JButton("Import Training QUIP"),
+        JButton("Randomize Assignments"), JButton("Export Roster"))
 
-    roster.importSSPOT(showOpenDialog())
-    roster.importTrainingQUIP(showOpenDialog())
-    roster.randomizeRoles()
+    buttonList[0].addActionListener { roster.importSSPOT(showOpenDialog()) }
+    buttonList[1].addActionListener { roster.importTrainingQUIP(showOpenDialog()) }
+    buttonList[2].addActionListener { roster.randomizeRoles() }
+    buttonList[3].addActionListener { roster.exportRoster(showSaveDialog()) }
+
+    window.contentPane.layout = GridLayout(buttonList.size, 1)
+    for(button in buttonList)
+        window.contentPane.add(button)
+
+    //roster.importSSPOT(showOpenDialog())
+    //roster.importTrainingQUIP(showOpenDialog())
+    //roster.randomizeRoles()
 
     // Setting window parameters
     window.size = Dimension(400, 200)
-    window.jMenuBar = menuBar
     window.isResizable = false
     window.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
     window.isVisible = true
