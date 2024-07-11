5229a922f3251447bff21799a9615612aead70fe
cagivens <ezio2468onpsn@gmail.com>
2024-07-10 20:28:07 -0400

    Deleting watchdog classes with intent to simplify in the future

--- src/Shodan.java
+++ src/Shodan.java
@@ -3,9 +3,6 @@ import rosterizer.Roster;
 import javax.swing.*;
 import javax.swing.filechooser.FileSystemView;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
@@ -55,6 +52,10 @@ public class Shodan {
         } catch(IOException e) {
             System.out.println("IOException thrown in Shodan.writeBytesToFile");
         }
+    }
+
+    public static byte[] readBytesFromFile(String filepath) {
+        return new byte[0];
     }
 
     public static String showSaveDialog() {
