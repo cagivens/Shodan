93b6d41761a8bf23dff4c7ff1570fab2fc56f465
Caleb Givens <ezio2468onpsn@gmail.com>
2024-07-12 11:34:34 -0400

    Added File IO functionality to Shodan.java

--- src/Shodan.java
+++ src/Shodan.java
@@ -1,11 +1,17 @@
+import net.bytebuddy.dynamic.scaffold.MethodGraph;
 import rosterizer.Roster;
 
 import javax.swing.*;
 import javax.swing.filechooser.FileSystemView;
 import java.awt.*;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Shodan {
 
@@ -16,8 +22,8 @@ public class Shodan {
 
         List<JButton> buttonList = List.of(new JButton("Import SSPOT"), new JButton("Import Training QUIP"),
                 new JButton("Randomize Assignments"), new JButton("Export Roster"));
-        buttonList.get(0).addActionListener(e -> roster.importSSPOT(showOpenDialog()));
-        buttonList.get(1).addActionListener(e -> roster.importTrainingQUIP(showOpenDialog()));
+        buttonList.get(0).addActionListener(e -> roster.importSSPOT(readCSVFile(showOpenDialog())));
+        buttonList.get(1).addActionListener(e -> roster.importTrainingQUIP(readCSVFile(showOpenDialog()), "NIT"));
         buttonList.get(2).addActionListener(e -> roster.randomizeRoles());
         buttonList.get(3).addActionListener(e -> writeBytesToFile(showSaveDialog(), roster.toString().getBytes()));
 
@@ -35,15 +41,6 @@ public class Shodan {
         // Function checks the local dir for an internal trained roles file
     }
 
-    public static void openRosterizer(Roster roster) {
-        JFrame rosterizerWindow = new JFrame();
-        rosterizerWindow.setVisible(true);
-    }
-
-    public static void initializeWatchdogs() {
-
-    }
-
     public static void writeBytesToFile(String filepath, byte[] bytes) {
         try {
             FileOutputStream outStream = new FileOutputStream(filepath);
@@ -54,9 +51,23 @@ public class Shodan {
         }
     }
 
-    public static byte[] readBytesFromFile(String filepath) {
-        return new byte[0];
+    public static List<String> readCSVFile(String filepath) {
+        LinkedList<String> rows = new LinkedList<>();
+
+        try {
+            File file = new File(filepath);
+            Scanner scanner = new Scanner(file);
+
+            while(scanner.hasNextLine())
+                rows.add(scanner.nextLine());
+        } catch(FileNotFoundException e) {
+            System.out.printf("File at '%s' not found", filepath);
+        }
+
+        return rows;
     }
+
+    public static int randomInt(int max) { return new Random().nextInt(max); }
 
     public static String showSaveDialog() {
         JFileChooser chooser = new JFileChooser(FileSystemView.getFileSystemView());
